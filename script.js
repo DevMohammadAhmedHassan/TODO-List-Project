d573c28c76299134f35261a98a68fe94ced49361
DevMohammadAhmedHassan <mohammadahmedhassan5@gmail.com>
2026-07-22 16:59:52 +0500

    js done

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let submitBtn = document.querySelector(".input button");
 let clearAllBtn = document.querySelector(".clearAll");
 let taskList = document.querySelector("ul");
 
+
+
+
 // Functions
 
 const addTask = () => {
@@ -19,10 +22,10 @@ const addTask = () => {
   newItem.innerHTML = `
               <p>${task}</p>
               <div>
-                <button>
+                <button class = "check">
                   <i class="fa-solid fa-check" style="color: rgb(0, 209, 70)"></i>
                 </button>
-                <button>
+                <button class = "delete">
                   <i
                     class="fa-regular fa-trash-can"
                     style="color: rgb(211, 0, 0)"
@@ -32,14 +35,29 @@ const addTask = () => {
 
   taskList.appendChild(newItem); // to add the new item.
 
+  inputField.value = ""; // Clear the input field
+  inputField.focus();  //user can automatically type the nex task without clicking.
+
   console.log("submit button clicked");
+
+  let checkTask = newItem.querySelector(".check"); //tick button
+  let deleteTask = newItem.querySelector(".delete"); //delete button
+
+  checkTask.addEventListener("click", () => {
+    newItem.querySelector("p").style.textDecoration = "line-through";
+  });
+
+  deleteTask.addEventListener("click", () => taskList.removeChild(newItem));
 };
+
 
 const clearAllTasks = () => {
     taskList.innerHTML = ""; //to clear the tasks!
 
     console.log("All Tasks Cleared");
 };
+
+
 
 // Event Listeners
 submitBtn.addEventListener("click", addTask);
