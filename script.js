73d178aed3107a7de44bd4d7563338a50404e70e
DevMohammadAhmedHassan <mohammadahmedhassan5@gmail.com>
2026-07-22 18:20:51 +0500

    added clear All button Animation

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ let taskList = document.querySelector("ul");
 // Functions
 
 const addTask = () => {
-  let task = inputField.value;
+    let task = inputField.value;
 
-  if (task == "") {
-    console.log("Empty input"); //to avoid empty input
-    return;
-  }
+    if (task == "") {
+        console.log("Empty input"); //to avoid empty input
+        return;
+    }
 
-  let newItem = document.createElement("li"); // to create a new item
+    let newItem = document.createElement("li"); // to create a new item
 
-  newItem.innerHTML = `
+    newItem.innerHTML = `
               <p>${task}</p>
               <div>
                 <button class = "check">
@@ -30,37 +30,45 @@ const addTask = () => {
                 </button>
               </div>`;
 
-  taskList.appendChild(newItem); // to add the new item.
-
-  setTimeout(() => {
-    newItem.classList.add("show");
-  }, 0);
-
-  inputField.value = ""; // Clear the input field
-  inputField.focus(); //user can automatically type the nex task without clicking.
-
-  console.log("submit button clicked");
-
-  let checkTask = newItem.querySelector(".check"); //tick button
-  let deleteTask = newItem.querySelector(".delete"); //delete button
-
-  checkTask.addEventListener("click", () => {
-    newItem.querySelector("p").style.textDecoration = "line-through";
-  });
-
-  deleteTask.addEventListener("click", () => {
-    newItem.classList.remove("show");  //for animation to work
+    taskList.appendChild(newItem); // to add the new item.
 
     setTimeout(() => {
-      taskList.removeChild(newItem);
-    }, 230);                           //delay for animation
-  });
+        newItem.classList.add("show");
+    }, 0);
+
+    inputField.value = ""; // Clear the input field
+    inputField.focus(); //user can automatically type the nex task without clicking.
+
+    console.log("submit button clicked");
+
+    let checkTask = newItem.querySelector(".check"); //tick button
+    let deleteTask = newItem.querySelector(".delete"); //delete button
+
+    checkTask.addEventListener("click", () => {
+        newItem.querySelector("p").style.textDecoration = "line-through";
+    });
+
+    deleteTask.addEventListener("click", () => {
+        newItem.classList.remove("show");  //for animation to work
+
+        setTimeout(() => {
+            taskList.removeChild(newItem);
+        }, 230);                           //delay for animation
+    });
 };
 
 const clearAllTasks = () => {
-  taskList.innerHTML = ""; //to clear the tasks!
+    let allTasks = taskList.querySelectorAll("li");
 
-  console.log("All Tasks Cleared");
+    allTasks.forEach((li) => {
+        li.classList.remove("show");
+    });
+
+    setTimeout(() => {
+        taskList.innerHTML = ""; //to clear the tasks!
+    }, 230);
+
+    console.log("All Tasks Cleared");
 };
 
 // Event Listeners
